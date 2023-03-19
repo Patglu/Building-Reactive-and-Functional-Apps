1e440dec3c1dcce5c0d45f976bd9aea06b2095e4
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-19 15:57:18 +0200

    Using prepend

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
+import Combine
 
-var greeting = "Hello, playground"
+///Appening values before the intials values
+
+let numbers = (1...5).publisher
+let secondNumebrs = (200...205).publisher
+
+numbers.prepend(100,101)
+    .prepend(-1,-2)
+    .prepend([45,67])
+    .prepend(secondNumebrs)
+    .sink {
+    print($0)
+}
