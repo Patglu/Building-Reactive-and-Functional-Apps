5f5281f15bc7a73d6161b17fbbd9d050db116950
Mphathi Maapola <113903905+Patglu@users.noreply.github.com>
2023-03-19 15:59:00 +0200

    Using append

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -1,15 +1,24 @@
 import UIKit
 import Combine
 
+//MARK: - Prepend
 ///Appening values before the intials values
 
-let numbers = (1...5).publisher
-let secondNumebrs = (200...205).publisher
+//let numbers = (1...5).publisher
+//let secondNumebrs = (200...205).publisher
+//
+//numbers.prepend(100,101)
+//    .prepend(-1,-2)
+//    .prepend([45,67])
+//    .prepend(secondNumebrs)
+//    .sink {
+//    print($0)
+//}
 
-numbers.prepend(100,101)
-    .prepend(-1,-2)
-    .prepend([45,67])
-    .prepend(secondNumebrs)
+//MARK: - Append
+let numbers = (1...10).publisher
+numbers.append(11,12)
+    .append(14,14)
     .sink {
-    print($0)
-}
+        print($0)
+    }
